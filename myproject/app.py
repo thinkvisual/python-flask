c78854ea050042e70d2d7184afaa6f1decc18d6e
David Howell <thinkvisual@hotmail.co.uk>
2023-01-21 23:34:03 +0000

    changes

--- myproject/app.py
+++ myproject/app.py
@@ -1,7 +1,12 @@
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
